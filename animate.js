d62efd0fa46811393e40f4da806ceb903fcaeb58
Melanie West <dhmofan@gmail.com>
2017-09-03 13:42:08 -0400

    Add buttons and recognize when a button is clicked

--- animate.js
+++ animate.js
@@ -1,58 +1,106 @@
-//$(document).ready(function(){
+$(document).ready(function(){
 
-    console.log("We are here!!");
+    var animals = [];  //array set up for buttons
+    var clickedAnimal;  //variable for setting up ajax request
 
-//start with a blank array for buttons
- var animalArray = [];
-console.log(animalArray);
-
-  //listen for click of button id 'addAnimal'.  When it is clicked, assign the value
-  //of the form input to the array and call 'render buttons'
-
-$("#addAnimal").on("click",function(event){
-
-    event.preventDefault;
-    var input = $("#animal-input").val().trim();
-    console.log(event.key, input);
+    // --------function that dynamically renders buttons with data to the document -----------------
+          function renderButtons() {
     
-    animalArray.push(input);
-
-    console.log(animalArray);
-
-    $("#animal-input").val(' ');  //empty the box
-
-    // The renderButtons function is called, rendering the list of movie buttons
-    return animalArray;
-
-    renderButtons();
-  });       //end of add animal routine
-
-
-
-//this function will create buttons with name of animal as an attribute of the button  
+          $("#animals-view").empty();
     
-    function renderButtons() {
+          for(var i= 0; i<animals.length; i++){
+              var b = $('<button>');
+                b.addClass('animals');
+                b.attr('data-name',animals[i]);
+                b.text(animals[i]);
+    
+       //       var button = "<button>"+animals[i]+'</button>';
+              console.log('button attr ='+$(b).attr('data-name'))
+
+              $("#animals-view").append(b);
+    
+              console.log(animals[i]);
+            }  
+          }
+    // -----------------end of render buttons function
+    
+    // --------------begin function that adds to my 'animal array', used for button rendering ------------
+
+          // This function handles events where the add animal button is clicked
+          $("#add-animal").on("click", function(event) {
+    
+                //prevents submit button from trying to send a form.
+                event.preventDefault();
+                var input = $("#animal-input").val().trim();
         
+                animals.push(input);
+                console.log(animals);
+                $("#animal-input").val(' ');  //empty the box
         
-        $("#animal-buttons").empty();
+                // The renderButtons function is called, rendering the list of animal buttons
+                renderButtons();
 
-        for(var i= 0; i<animalArray.length; i++){
-            var b = $('<button>');
-            b.addClass('animal');
-            b.attr('data-name',animalArray[i]);
-            b.text(animalArray[i]);
-
-            $("#animal-buttons").append(b);
-
-            console.log(animals[i]);
-        }
+            });
+//  -----------------end function that adds to animal array-----------------------
 
 
-        // Loop through the array of animals, then generate buttons for each animal in the array
+//-----------------begin function for displaying gifs for each animal
+        
 
-        }   //end of render buttons loop
+    function displayGifs(){
 
-        renderButtons();
+         clickedAnimal = $(this).attr("data-name");   //name is extracted from button
+         console.log(clickedAnimal);
 
-//});      //end of document ready
+ //       event.preventDefault;
+
+        console.log("you clicked a button");
+
+
+        // var animalTag = '&tag='+ clickedAnimal;
+        // var queryURL = "https://api.giphy.com/v1/gifs/random?&api_key=dc6zaTOxFJmzC" + animalTag;
+        // //  console.log(animal, animalTag);
+
+        var queryURL = "https://api.giphy.com/v1/gifs/random?&api_key=dc6zaTOxFJmzC&tag=cats"        
+
+          $.ajax({        
+            url: queryURL,
+            method: "GET"
+          }).done(function(response) {
+            console.log(response);      //'response' is all the info the api has on my animal
+         
+            var animalDiv = $('<div class="gifs">');
+            
+            var imgURL = response.data.image_original_url;
+            var animalImg = $('<img>')
+            animalImg.attr("src",imgURL);
+            animalDiv.append(animalImg);
+
+            // var released = response.Released;
+            // var pTwo = $('<p>').text("Released: " + released);
+            // animalDiv.append(pTwo);
+
+        
+            // var plot = response.Plot;
+            // var pThree = $('<p>').text("Plot: " + plot);
+            // animalDiv.append(pThree);
+
+        
+            // var imgURL = response.Poster;
+            // var image = $('<img>').attr('src',imgURL);
+            // animalDiv.append(image);
+
+            $('#animal-gifs').append(animalDiv);  //add to front rather than back
+        
+
+
+          });       //end of response to ajax request
+
+
+    }   //end of button click event/ function
+
+    $(document).on("click","#animals-view", displayGifs);
+    
+
+});      //end of document ready
 
